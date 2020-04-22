0e1690e798614adc8197d4d53ca9f76784b69e05
SharkSix <62574991+SharkSix@users.noreply.github.com>
2020-04-21 22:26:20 -0600

    security
    Remove connection string from source code

--- Data/StrongBartendingContext.cs
+++ Data/StrongBartendingContext.cs
@@ -40,12 +40,7 @@ namespace StrongBartending.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                //MING
-                optionsBuilder.UseSqlServer("Server=(localdb)\\SQLEXPRESS;Database=StrongBartending; Trusted_Connection=True");
 
-                ////MARK
-                //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=StrongBartending;Trusted_Connection=True;");
             }
         }
 
